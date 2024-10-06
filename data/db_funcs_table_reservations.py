be8aff496f9cc016cb8061e7b414474f9a84cf03
dblmok <dblmokdima@gmail.com>
2024-10-06 04:16:04 +0300

    start

--- data/db_funcs_table_reservations.py
+++ data/db_funcs_table_reservations.py
@@ -14,11 +14,11 @@ def load_table_reservations(date: datetime = None) -> str:
     try:
         with (db_beahea):
             if date:
-                datas = TableReservationHistory.select().where(TableReservationHistory.booking_start_time.to_timestamp()
+                datas = TableReservationHistory.select().where(TableReservationHistory.booking_start_time
                                                                == date.date()
                                                                ).order_by(TableReservationHistory.booking_start_time)
             else:
-                datas = TableReservationHistory.select().where(TableReservationHistory.booking_start_time.to_timestamp()
+                datas = TableReservationHistory.select().where(TableReservationHistory.booking_start_time.cast('date')
                                                                >= datetime.now().date()
                                                                ).order_by(TableReservationHistory.booking_start_time)
             if datas:
@@ -45,23 +45,3 @@ def load_table_reservations(date: datetime = None) -> str:
                       f'Ошибка: {exp}')
         return f'В процессе загрузки резервов столов произошла непредвиденная ошибка\nОшибка: {exp}'
 
-
-with (db_beahea):
-    datas = TableReservationHistory.select().where(TableReservationHistory.booking_start_time.to_timestamp()
-                                                   >= datetime.now().date()
-                                                   ).order_by(TableReservationHistory.booking_start_time)
-
-    for data in datas:
-        id = data.id
-        table = data.table.number_table
-        number_of_guests = data.number_of_guests
-        booking_start_time = data.booking_start_time.strftime('%d-%m-%Y %H:%M')
-        if data.phone_number:
-            phone = data.phone_number
-        else:
-            phone = data.user.phone
-        print(f'id резерва: {id}\n'
-              f'Номер стола: {table}\n'
-              f'Количество гостей: {number_of_guests}\n'
-              f'Дата и время резерва: {booking_start_time}\n'
-              f'Телефон: {phone}\n\n')
